9c3c1c2e562e0eb814190a978d7e939088f36250
Wirre de Vries <wirredevries@Wirres-MBP.lan>
2015-09-16 09:15:07 +0200

    Eerste opzetje voor de klankinstallatie opdracht; 5 seconden puur geluid.

---
+++ puresound.sh
@@ -0,0 +1,20 @@
+#1 /bin/bash
+
+#dit opent eerst de map met fragmenten
+
+cd ~/SO2_folder/klankinstallatie/wav_files/
+
+#dit pakt de eerste 5 seconden van het fragment, en voegt 0.1 s fade in en -out toe
+
+find . -type f -name "*.wav" -exec sh -c '
+origineel="$0"
+bewerkt="$(`basename $0 .wav`puresound.wav)"
+sox $origineel $bewerkt trim 0.0 5.0 fade 0.1 0.1
+' {} \;
+
+#maakt submap aan en verplaatst de file naar submapje met de geprocesste files
+
+mkdir pure_sound
+mv $bewerkt $bewerkt
+
+
