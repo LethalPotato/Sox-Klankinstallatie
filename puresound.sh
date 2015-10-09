83bc2e5245b7be093463bc274785e43a5fa4af8d
Wirre de Vries <wirredevries@Wirres-MBP.lan>
2015-10-09 11:36:20 +0200

    Alright, ik liep een beetje te hard van stapel. Dit keer gewoon basename gebruikt ipv. meteen de hele filename in een abstraction te smijten. Nu eens kijken oftie werkt.
    *echo nog weghalen
    *verdergaan met bewerkingen als ie t doet.

--- puresound.sh
+++ puresound.sh
@@ -8,13 +8,13 @@ cd ~/SO2_folder/klankinstallatie/wav_files/
 
 find . -type f -name "*.wav" -exec sh -c '
 origineel="$0"
-bewerkt="$(`basename $0 .wav`puresound.wav)"
-sox $origineel $bewerkt trim 0.0 5.0 fade 0.1 0.1
+basename=`basename $0 .wav`
+echo sox $origineel ${basename}_pure.wav trim 0.0 5.0 fade 0.1 0.1
 ' {} \;
 
 #maakt submap aan en verplaatst de file naar submapje met de geprocesste files
 
 mkdir pure_sound
-mv $bewerkt $bewerkt
+mv ${basename}_pure.wav ../pure_sound/${basename}_pure.wav
 
 
