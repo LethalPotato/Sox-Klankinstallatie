71bca28639c546009262e474f6a2c3ff82ac3b26
Wirre de Vries <wirredevries@Wirres-MBP.lan>
2015-10-09 14:50:56 +0200

    yes! Typos weggewerkt, puur geluid en 'kamer verderop' werken prima.
    To do:
    
    *distortion
    *eigen invulling
    *is niet handig dat alles in losse folders staat voor 1 minuut random stukjes (ben bang dat me dat uberhaupt niet lukt)

--- sox_klank_script.sh
+++ sox_klank_script.sh
@@ -1,27 +1,60 @@
-#! /bin/bash
-
 #open folder met mp3s
 
 cd sox_fragments 
 
-#vind alle mp3s, converteer ze naar .wav file in wav_files
+#vind alle mp3s, converteer ze naar .wav
 
-find . -name "*.mp3" -exec '
+find . -name "*.mp3" -exec sh -c '
 mp3="$0"
 basename=`basename $0 .mp3`
-sox --norm $mp3 -r 44100 ../wav_files/${basename}.wav
+sox $mp3 -r 44100 ${basename}.wav norm
 ' {} \;
 
-#nu komt het leuke werk, we gaan met de files kloten, maar eerst openen we het mapje met de wave files
+#Zet ze in mapje wav_files op hetzelfde niveau als sox_fragments
+
+mkdir ../wav_files
+
+find . -name "*.wav" -exec sh -c '
+file="$0"
+mv $file ../wav_files/${file}
+' {} \;
+
+#nu komt het leuke werk, we gaan met de files kloten, maar eerst openen we het mapje met de wave files en maken we een mapje voor het pure geluid
 
 cd ../wav_files
 
-find . -name "*.wav" -exec '
-origineel=$0
-basename=`basename $0 .wav"
-sox $origineel ../puurgeluid/${basename}_puur.wav trim 0.0 5.0 fade 0.1 0.1
+find . -name "*.wav" -exec sh -c '
+origineel="$0"
+basename=`basename $0 .wav`
+sox --norm $origineel ${basename}_puur.wav trim 0 5 fade 0.1 4.8 0.1
 ' {} \;
 
 
+#Zet ze in mapje puurgeluid
+
+mkdir ../puurgeluid
+
+find . -name "*_puur.wav" -exec sh -c '
+file="$0"
+mv $file ../puurgeluid/${file}
+' {} \;
+
+
+#En nu gaan we filteren! ('alsof het ergens anders in het gebouw klinkt')
+
+find . -name "*.wav" -exec sh -c '
+origineel="$0"
+basename=`basename $0 .wav`
+sox --norm $origineel ${basename}_lowpass.wav vol 0.5 lowpass 1000.0 trim 0 4 fade 0.1 3.8 0.1
+' {} \;
+
+#ook deze krijgen hun eigen mapje:
+
+mkdir ../lowpass
+
+find . -name "*_lowpass.wav" -exec sh -c '
+file="$0"
+mv $file ../lowpass/${file}
+' {} \;
 
 
